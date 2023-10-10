7507f00d6c880704039a4196baa52877148557f7
Aleksey Veresov <aleksey@veresov.pro>
2023-10-10 11:18:35 +0200

    Kinda works

--- lab3-skel/src/Experiment.java
+++ lab3-skel/src/Experiment.java
@@ -5,65 +5,81 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
-public class Experiment {
-	private static final int MAX_NUMBER = 100_000;
+public class Experiment
+{
+    private static final int MAX_NUMBER = 100_000;
 
-	public static long run_experiment(int threads, int count, LockFreeSet<Integer> list, Distribution ops, Distribution values) throws Exception {
-		ExecutorService executorService = Executors.newFixedThreadPool(threads);
+    public static long run_experiment(int threads, int count, LockFreeSet<Integer> list, Distribution ops,
+            Distribution values) throws Exception
+    {
+        ExecutorService executorService = Executors.newFixedThreadPool(threads);
 
-		Task[] tasks = new Task[threads];
-		for (int i = 0; i < tasks.length; ++i) {
-			tasks[i] = new Task(i, count, list, ops.copy(i), values.copy(-i));
-		}
+        Task[] tasks = new Task[threads];
+        for (int i = 0; i < tasks.length; ++i)
+        {
+            tasks[i] = new Task(i, count, list, ops.copy(i), values.copy(-i));
+        }
 
-		long startTime = System.nanoTime();
-		executorService.invokeAll(Arrays.asList(tasks));
-		long endTime = System.nanoTime();
-		executorService.shutdown();
+        long startTime = System.nanoTime();
+        executorService.invokeAll(Arrays.asList(tasks));
+        long endTime = System.nanoTime();
+        executorService.shutdown();
 
-		return endTime - startTime;
-	}
+        return endTime - startTime;
+    }
 
-	public static class Task implements Callable<Void> {
-		private final int threadId, count;
-		private final LockFreeSet<Integer> set;
-		private final Distribution ops, values;
+    public static class Task implements Callable<Void>
+    {
+        private final int threadId, count;
+        private final LockFreeSet<Integer> set;
+        private final Distribution ops, values;
 
-		public Task(int threadId, int count, LockFreeSet<Integer> set, Distribution ops, Distribution values) {
-			this.threadId = threadId;
-			this.set = set;
-			this.ops = ops;
-			this.values = values;
+        public Task(int threadId, int count, LockFreeSet<Integer> set, Distribution ops, Distribution values)
+        {
+            this.threadId = threadId;
+            this.set = set;
+            this.ops = ops;
+            this.values = values;
             this.count = count;
-		}
+        }
 
-		public Void call() throws Exception {
-			for (int i = 0; i < count; ++i) {
-				int val = values.next();
-				int op = ops.next();
-				switch (op) {
-					case 0:
-						set.add(threadId, val);
-						break;
-					case 1:
-						set.remove(threadId, val);
-						break;
-					case 2:
-						set.contains(threadId, val);
-						break;
-				}
-			}
-			return null;
-		}
-	}
+        public Void call() throws Exception
+        {
+            for (int i = 0; i < count; ++i)
+            {
+                int val = values.next();
+                int op = ops.next();
+                switch (op)
+                {
+                case 0:
+                    set.add(threadId, val);
+                    break;
+                case 1:
+                    set.remove(threadId, val);
+                    break;
+                case 2:
+                    set.contains(threadId, val);
+                    break;
+                }
+            }
+            return null;
+        }
+    }
 
-	public static void main(String[] args) {
-		for (Distribution values : new Distribution[]{new Distribution.Uniform(84, 0, MAX_NUMBER), new Distribution.Normal(84,15,0,MAX_NUMBER)}) {
-            for (int[] distr : new int[][]{new int[]{1, 1, 8}, new int[]{1, 1, 0}}) {
-                for (int num_threads : new int[]{1, 2, 4, 8, 16, 32, 64, 96}) {
-                    int warmup = 9;
-                    for(int i=0;i<warmup+1;i++) {
-                        try {
+    public static void main(String[] args)
+    {
+        for (Distribution values : new Distribution[] { new Distribution.Uniform(84, 0, MAX_NUMBER),
+                new Distribution.Normal(84, 15, 0, MAX_NUMBER) })
+        {
+            for (int[] distr : new int[][] { new int[] { 1, 1, 0 }, new int[] { 1, 1, 0 } })
+            {
+                for (int num_threads : new int[] { 1, 2, 4, 8, 16, 32, 64, 96 })
+                {
+                    int warmup = 0;
+                    for (int i = 0; i < warmup + 1; i++)
+                    {
+                        try
+                        {
                             // Create a standard lock free skip list
                             LockFreeSet<Integer> lockFreeSet = new LockFreeSkipList<>();
 
@@ -73,16 +89,18 @@ public class Experiment {
 
                             // Run experiment with 16 threads.
                             long time = run_experiment(num_threads, MAX_NUMBER, lockFreeSet, ops, values);
-                            if(i==warmup) // after warmup
-                                System.out.printf("Took %f ms to finish for %d workers.\n", (time)/1_000_000.0f, num_threads);
-                                
-                            
+                            if (i == warmup) // after warmup
+                                System.out.printf("Took %f ms to finish for %d workers.\n", (time) / 1_000_000.0f,
+                                        num_threads);
+
                             // Get the log
                             Log.Entry[] log = lockFreeSet.getLog();
 
                             // Check sequential consistency
                             Log.validate(log);
-                        } catch (Exception e) {
+                        }
+                        catch (Exception e)
+                        {
                             e.printStackTrace();
                             System.exit(1);
                         }
--- lab3-skel/src/Log.java
+++ lab3-skel/src/Log.java
@@ -1,14 +1,17 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Log {
-	private Log() {
-		// Do not implement
-	}
+public class Log
+{
+    private Log()
+    {
+        // Do not implement
+    }
 
-	public static boolean validate(Log.Entry[] log) 
+    public static boolean validate(Log.Entry[] log)
     {
         Set<Integer> seqSet = new HashSet<>();
 
@@ -16,52 +19,64 @@ public class Log {
         Entry[] sortedLog = Arrays.copyOf(log, log.length);
         Arrays.sort(sortedLog, Comparator.comparingLong(entry -> entry.timestamp));
 
-
-        for (Log.Entry logi:sortedLog) 
+        int i = 0;
+        for (Log.Entry logi : sortedLog)
         {
-			int val = Integer.parseInt(logi.argument); // TODO think
-			boolean resSeq;
-			switch (logi.method) 
+            int val = Integer.parseInt(logi.argument); // TODO think
+            boolean resSeq;
+            switch (logi.method)
             {
             case ADD:
-				resSeq = seqSet.add(val);
+                resSeq = seqSet.add(val);
                 break;
             case REMOVE:
-				resSeq = seqSet.remove(val);
+                resSeq = seqSet.remove(val);
                 break;
             case CONTAINS:
-				resSeq = seqSet.contains(val);
-				break;
+                resSeq = seqSet.contains(val);
+                break;
             default:
                 throw new RuntimeException("Unknown method: " + logi.method);
-			}
-			if (resSeq == logi.retval)
-				continue;
-			System.out.println(": value of lock free (" + logi.retval + ") not matching sequential (" + resSeq + ")");
+            }
+            i++;
+            if (resSeq == logi.retval)
+                continue;
+            System.out.println(i + " " + logi.method.toString() + "(" + logi.argument + "): value of lock free ("
+                    + logi.retval + ") not matching sequential (" + resSeq + ")");
             return false;
         }
-		return true;
-	}
-    
-    
-    public static enum Method { ADD, REMOVE, CONTAINS };
+        return true;
+    }
 
-	// Log entry for linearization point.
-	public static class Entry {
+    public static enum Method
+    {
+        ADD, REMOVE, CONTAINS
+    };
+
+    // Log entry for linearization point.
+    public static class Entry
+    {
         int threadId;
         Method method;
         String argument;
         boolean retval;
         long timestamp;
-		public Entry(int threadId, Method method, String argument, boolean retval, long timestamp) 
+
+        public Entry(int threadId, Method method, String argument, boolean retval, long timestamp)
         {
             this.threadId = threadId;
             this.method = method;
             this.argument = argument;
             this.retval = retval;
             this.timestamp = timestamp;
-            
-			// TODO add method, arguments, return value and timestamp.
-		}
-	}
+
+            // TODO add method, arguments, return value and timestamp.
+        }
+
+        public String toString()
+        {
+            return "Entry{" + "threadId=" + threadId + ", method=" + method + ", argument='" + argument + '\''
+                    + ", retval=" + retval + ", timestamp=" + timestamp + '}';
+        }
+    }
 }
